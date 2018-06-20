4c2629b70e9d48365c1d262d3427e0d6f69f918d
Unknown <l.bourdrel@laposte.net>
2018-06-20 21:31:37 +0200

    Part 2 - Basic Architecture

---
+++ BreakTheLoop/Assets/02_Scripts/LD Events/Receptacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receptacle : MonoBehaviour {
+
+    public GameObject Trail_01;
+    public GameObject Trail_02;
+    public float activationDelay;
+
+    public Animator animDoor;
+
+
+    //DEBUG
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(Activation());
+        }     
+    }
+    //DEBUG
+
+    IEnumerator Activation()
+    {
+        Trail_01.SetActive(true);
+        Trail_02.SetActive(true);
+        Trail_01.GetComponent<TrailMove>().MoveToPoint();
+        Trail_02.GetComponent<TrailMove>().MoveToPoint();
+
+        yield return new WaitForSeconds(activationDelay);
+
+        animDoor.enabled = true;
+    }
+}
---
+++ BreakTheLoop/Assets/02_Scripts/LD Events/TrailMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TrailMove : MonoBehaviour {
+
+    public Transform[] wayPoints;
+    public float timeToGoToPoint;
+
+    Transform actualWaypoint;
+    int index = 0;
+
+    private void Start()
+    {
+        actualWaypoint = wayPoints[index];
+    }
+
+    public void MoveToPoint()
+    {
+        transform.DOMove(actualWaypoint.position, timeToGoToPoint).OnComplete(() =>
+        {
+            if (wayPoints[index + 1] != null)
+            {
+                index++;
+                actualWaypoint = wayPoints[index];
+                MoveToPoint();
+            }
+        });
+    }
+}
