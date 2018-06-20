42e997a6078a1182c249f21b545d04f3df6368b8
Unknown <l.bourdrel@laposte.net>
2018-06-20 19:51:21 +0200

    New room up visual

---
+++ BreakTheLoop/Assets/02_Scripts/LD Events/Red_CloseDoorBehind.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Red_CloseDoorBehind : MonoBehaviour {
+
+    public GameObject popObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popObject.SetActive(true);
+        }
+    }
+
+}
